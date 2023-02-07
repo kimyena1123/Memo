2c29430e7d6fc899122f8028bbd58ba4527db6b4
kimyena1123 <kyq104@gmail.com>
2023-02-07 19:08:26 +0900

    feat: first commit

---
+++ src/main/java/com/yena/memo/MemoApplication.java
@@ -0,0 +1,13 @@
+package com.yena.memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoApplication.class, args);
+	}
+
+}
